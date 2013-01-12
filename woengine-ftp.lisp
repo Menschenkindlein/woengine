6e3d9393bc66db0f9d246ea5da91ebc8286ca791
Zholobak Maxim <ghostcheka@gmail.com>
2013-01-12 16:46:42 +0200

    Some API improvements

--- woengine-ftp.lisp
+++ woengine-ftp.lisp
@@ -15,7 +15,7 @@
      (ftp:send-cwd-command conn (root ,storage))
      ,@body))
 
-(defmethod initialize empty ((storage ftp-storage))
+(defmethod initialize-empty ((storage ftp-storage))
   (ftp:with-ftp-connection (conn :hostname (host storage)
 				 :username (username storage)
 				 :password (password storage)
@@ -23,7 +23,7 @@
     (ftp:send-mkd-command conn (root storage))
     (ftp:send-cwd-command conn (root storage))
     (ftp:store-file conn
-		    (make-string-input-stream "")
+		    (make-string-input-stream "NIL")
 		    ".woengine-hashes"
 		    :type :ascii)))
 
@@ -31,53 +31,35 @@
   (slot-value storage 'digester))
 
 (defmethod retrieve-hashes ((storage ftp-storage))
-  (with-ftp-storage storage
-    (ftp:retrieve-file conn ".woengine-hashes" ".temp-woengine-hashes"))
-  (let ((hashes (with-open-file (file ".temp-woengine-hashes")
-		  (read file nil))))
-    (delete-file ".temp-woengine-hashes")
-    hashes))
+  (let ((hashes-file (make-string-output-stream)))
+    (with-ftp-storage storage
+      (ftp:retrieve-file conn ".woengine-hashes" hashes-file :type :ascii))
+    (read-from-string (get-output-stream-string hashes-file) nil)))
 
 (defmethod store-hashes (storage hashes)
-  (with-open-file (file ".temp-woengine-hashes"
-			:direction :output
-			:if-does-not-exist :create)
-    (print hashes file))
   (with-ftp-storage storage
-    (ftp:store-file conn ".temp-woengine-hashes" ".woengine-hashes"))
-  (delete-file ".temp-woengine-hashes"))
+    (ftp:store-file conn
+		    (make-string-input-stream
+		     (prin1-to-string hashes))
+		    ".woengine-hashes"
+		    :type :ascii)))
 
 (defmethod retrieve-files (storage local-root paths)
-  (if paths
-      (progn
-	(format t "Retrieving remote files: ~{~%~a~}~%~%" paths)
-	(with-ftp-storage storage
-	  (dolist (path paths)
-	    (ftp:retrieve-file conn path (merge-pathnames path local-root)))))
-      (format t "Local files are up to date.~%~%")))
+  (with-ftp-storage storage
+    (dolist (path paths)
+      (ftp:retrieve-file conn path (merge-pathnames path local-root)))))
 
 (defmethod store-files (storage local-root paths)
-  (if paths
-      (progn
-	(format t "Copying local files: ~{~%~a~}~%~%" paths)
-	(with-ftp-storage storage
-	  (dolist (path paths)
-	    (ftp:store-file conn (merge-pathnames path local-root) path))))
-      (format t "Remote files are up to date.~%~%")))
+  (with-ftp-storage storage
+    (dolist (path paths)
+      (ftp:store-file conn (merge-pathnames path local-root) path))))
 
 (defmethod make-remote-directories (storage paths)
-  (if paths
-      (progn
-	(format t "Making remote directories: ~{~%~a~}~%~%" paths)
-	(with-ftp-storage storage
-	  (dolist (path paths)
-	    (ftp:send-mkd-command conn path))))
-      (format t "Remote directories are up to date.~%~%")))
+  (with-ftp-storage storage
+    (dolist (path paths)
+      (ftp:send-mkd-command conn path))))
 
 (defmethod delete-remote (storage paths)
-  (when paths
-    (progn
-      (format t "Deleting remote paths: ~{~%~a~}~%~%" paths)
-      (with-ftp-storage storage
-	(dolist (path paths)
-	  (ftp:send-dele-command conn path))))))
+  (with-ftp-storage storage
+    (dolist (path paths)
+      (ftp:send-dele-command conn path))))
